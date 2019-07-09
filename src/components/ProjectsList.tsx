66c81f1696b8d58ff3dd2061179b4c057212a7fa
Hexin <hexium310@gmail.com>
2019-07-10 03:58:22 +0900

    Change grid row height

--- src/components/ProjectsList.tsx
+++ src/components/ProjectsList.tsx
@@ -10,9 +10,9 @@ const Title = styled.h2`
 
 const ProjectsContainer = styled.div`
   display: grid;
-  grid-template-rows: 200px;
+  grid-template-rows: minmax(200px, 1fr);
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-  grid-auto-rows: 200px;
+  grid-auto-rows: minmax(200px, 1fr);
   grid-gap: 50px;
   justify-content: center;
   width: 90%;
