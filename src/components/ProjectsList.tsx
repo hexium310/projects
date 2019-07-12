bcdb4aa9f2747e8baa3e062f3544ed55c1a3c11f
Hexin <hexium310@gmail.com>
2019-07-13 03:27:51 +0900

    Add repository link of this

--- src/components/ProjectsList.tsx
+++ src/components/ProjectsList.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 import { ProjectItem } from './ProjectItem';
 import projects from '../../contents/projects.yaml';
 
+const Header = styled.div`
+  display: grid;
+  grid-template-columns: 4fr 1fr 4fr;
+  align-items: center;
+`;
+
 const Title = styled.h2`
+  grid-column: 2 / 3;
   text-align: center;
   font-family: 'Dosis', fantasy;
 `;
@@ -25,7 +34,16 @@ const ProjectsContainer = styled.div`
 export const ProjectsList: React.FunctionComponent = () => {
   return (
     <>
-      <Title>Projects</Title>
+      <Header>
+        <Title>
+          Projects
+        </Title>
+        <div>
+          <a href="https://github.com/hexium310/projects">
+            <FontAwesomeIcon icon={ faGithub } size='lg' />
+          </a>
+        </div>
+      </Header>
       <ProjectsContainer>
         {
           projects.map((project: Project, i: number) => (
