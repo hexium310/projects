1f8342dc359b64022b52e728fe489d5bcccd1b20
Hexin <hexium310@gmail.com>
2019-07-15 03:08:52 +0900

    Add memo

--- src/components/ProjectsList.tsx
+++ src/components/ProjectsList.tsx
@@ -17,14 +17,25 @@ const ProjectsContainer = styled.div`
   text-align: center;
 `;
 
+const Memo = styled.div`
+  margin: 20px;
+  color: rgba(0, 0, 0, 0.3);
+  font-size: 13px;
+`;
+
 export const ProjectsList: React.FunctionComponent = () => {
   return (
-    <ProjectsContainer>
-      {
-        projects.map((project: Project, i: number) => (
-          <ProjectItem project={ project } key={ i } />
-        ))
-      }
-    </ProjectsContainer>
+    <>
+      <ProjectsContainer>
+        {
+          projects.map((project: Project, i: number) => (
+            <ProjectItem project={ project } key={ i } />
+          ))
+        }
+      </ProjectsContainer>
+      <Memo>
+        静的サイトジェネレーター使わずに作ったらMarkdownの読み込みにfetch使う羽目になって厳しかったのでそのうちGatsbyで作り直します。
+      </Memo>
+    </>
   );
 };
